82bacb3b2f6395a1eb4a72f75cedba1bfa243824
huangzhibo <huangzhibo@genomics.cn>
2018-11-22 17:37:38 +0800

    change optional to required

--- src/main/java/org/bgi/flexlab/automation/json/WdlParameter.java
+++ src/main/java/org/bgi/flexlab/automation/json/WdlParameter.java
@@ -3,14 +3,13 @@ package org.bgi.flexlab.automation.json;
 public class WdlParameter {
     private final String type;
     private final Object defaultValue;
-    private final boolean optional;
+    private final boolean required;
     private final String description;
 
-    // defaultValue为空 且 optional == false, 则该参数必须在workflow中传入
-    public WdlParameter(String type, String defaultValue, boolean optional, String description) {
+    public WdlParameter(String type, String defaultValue, boolean required, String description) {
         this.type = type;
         this.defaultValue = defaultValue;
-        this.optional = optional;
+        this.required = required;
         this.description = description;
     }
 
@@ -22,8 +21,8 @@ public class WdlParameter {
         return defaultValue;
     }
 
-    public boolean isOptional() {
-        return optional;
+    public boolean isRequired() {
+        return required;
     }
 
     public String getDescription() {
